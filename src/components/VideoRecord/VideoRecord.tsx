0b1995e43c8fcb7c018d1e4f27259db9faa5413b
tamozhniy <p.tamozhniy@itcsolutions.ru>
2021-02-04 21:53:00 +0300

    1.10

--- src/components/VideoRecord/VideoRecord.tsx
+++ src/components/VideoRecord/VideoRecord.tsx
@@ -1,4 +1,7 @@
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Upload } from "antd";
 import Modal from "antd/lib/modal/Modal";
+import { RcFile } from "antd/lib/upload";
 import React from "react";
 import { ReactMediaRecorder } from "react-media-recorder";
 import styles from "./VideoRecord.module.css";
@@ -53,6 +56,14 @@ const VideoRecord: React.FC<IVideoRecord> = ({
   setVideo,
 }): React.ReactElement => {
   const [stat, setStat] = React.useState<string>("acquiring_media");
+  const [uploading, setUploading] = React.useState(false);
+  const [fileList, setFileList] = React.useState<Array<RcFile>>([]);
+  const onRemove = () => setFileList([]);
+  const beforeUpload = (file: RcFile): boolean => {
+    setFileList([file]);
+    return false;
+  };
+
   return (
     <Modal
       title={"Запись видео-ответа"}
@@ -70,70 +81,82 @@ const VideoRecord: React.FC<IVideoRecord> = ({
           setVideo(blobUrl);
         }}
         mediaRecorderOptions={{ status: stat }}
-        render={React.useCallback(
-          ({
-            status,
-            startRecording,
-            stopRecording,
-            pauseRecording,
-            resumeRecording,
-            mediaBlobUrl,
-            previewStream,
-            clearBlobUrl,
-          }) => {
-            setVideo(mediaBlobUrl);
-            setStat(status);
-            return (
-              <div>
-                {videoUrl && mediaBlobUrl ? (
-                  <video
-                    src={videoUrl}
-                    controls
-                    height={300}
-                    width={600}
-                    className={styles.video}
-                  />
-                ) : (
-                  <VideoPreview stream={previewStream} />
-                )}
-                <p>{getStatus(status)}</p>
-                <button onClick={startRecording}>Start Recording</button>
-                <button onClick={stopRecording}>Stop Recording</button>
-                <button
-                  onClick={() => {
-                    pauseRecording();
-                    status = "stopping";
-                    setStat(status);
-                  }}
-                >
-                  Pause Recording
-                </button>
-                <button
-                  onClick={() => {
-                    resumeRecording();
-                    status = "recording";
-                    setStat(status);
-                  }}
-                >
-                  Resume Recording
-                </button>
-                <button
-                  onClick={() => {
-                    clearBlobUrl();
-                    mediaBlobUrl = null;
-                    setVideo(undefined);
-                    status = "idle";
-                    setStat(status);
-                  }}
-                >
-                  Clear Recording
-                </button>
-              </div>
-            );
-          },
-          // eslint-disable-next-line react-hooks/exhaustive-deps
-          [stat, videoUrl]
-        )}
+        render={({
+          status,
+          startRecording,
+          stopRecording,
+          pauseRecording,
+          resumeRecording,
+          mediaBlobUrl,
+          previewStream,
+          clearBlobUrl,
+        }) => {
+          setVideo(mediaBlobUrl);
+
+          return (
+            <div>
+              {videoUrl && mediaBlobUrl ? (
+                <video
+                  src={videoUrl}
+                  controls
+                  height={300}
+                  width={600}
+                  className={styles.video}
+                />
+              ) : (
+                <VideoPreview stream={previewStream} />
+              )}
+              <p>{getStatus(status)}</p>
+              <button onClick={startRecording}>Start Recording</button>
+              <button onClick={stopRecording}>Stop Recording</button>
+              <button
+                onClick={() => {
+                  pauseRecording();
+                  status = "stopping";
+                  setStat(status);
+                }}
+              >
+                Pause Recording
+              </button>
+              <button
+                onClick={() => {
+                  resumeRecording();
+                  status = "recording";
+                  setStat(status);
+                }}
+              >
+                Resume Recording
+              </button>
+              <button
+                onClick={() => {
+                  clearBlobUrl();
+                  mediaBlobUrl = null;
+                  setVideo(undefined);
+                  status = "idle";
+                  setStat(status);
+                }}
+              >
+                Clear Recording
+              </button>
+              <Upload
+                fileList={fileList}
+                onRemove={onRemove}
+                beforeUpload={beforeUpload}
+              >
+                <Button icon={<UploadOutlined />}>Select File</Button>
+              </Upload>
+              <Button
+                type="primary"
+                onClick={() => setUploading(true)} // Сервис для отправки
+                disabled={fileList.length === 0}
+                loading={uploading}
+                style={{ marginTop: 16 }}
+              >
+                {uploading ? "Загружено" : "Начать загрузку"}
+              </Button>
+            </div>
+          );
+        }}
       />
     </Modal>
   );
